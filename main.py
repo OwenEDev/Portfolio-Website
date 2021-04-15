ce59bb9208451115481e60223bb6e34dffd2336b
OwenEDev <owen0879@gmail.com>
2021-04-15 14:58:15 +0100

    added contact info, links, and project panels to page 2

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def Page():
 def page():
     return render_template("page_2.html")
 
+# @app.route("/your_shopping_list")
+# def page():
+#     return render_template()
+
+# @app.route("/portfolio_site")
+# def page():
+#     return render_template()
+
 if __name__ == "__main__":
     app.run(debug=True)
